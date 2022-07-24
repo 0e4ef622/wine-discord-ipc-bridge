e5e78f6c3f331afa64cfeeb98c9a9a8bc690a4cf
Sammy <sammy@sammcheese.net>
2022-07-24 18:15:35 +0200

    Add /run/user/$UID to possible IPC Paths

--- winediscordipcbridge-steam.sh
+++ winediscordipcbridge-steam.sh
@@ -23,6 +23,7 @@ DISCORD_IPC_PATHS=(
     "$TEMP_PATH/app/com.discordapp.Discord"
     "$TEMP_PATH/snap.discord-canary"
     "$TEMP_PATH/snap.discord"
+    "run/user/$UID"
 )
 
 for ipc_path in "${DISCORD_IPC_PATHS[@]}"; do
